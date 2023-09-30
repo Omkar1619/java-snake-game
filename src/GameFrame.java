3444264b4818a869dbe459f62656c22a16a6056d
Omkar <9537732360omkar@gmail.com>
2023-09-30 16:01:50 +0530

    initial commit

---
+++ src/GameFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GameFrame(){
+//        System.out.println("gameframe ran");
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
